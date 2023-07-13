46b4a05ed8238c400db1772e10b94ac10c032586
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-07-13 16:16:54 +0600

    in player update function, added math.atan2 to find angle between mouse and player origin, and using the angle, change the frameX and show different image from sprite image

--- script.js
+++ script.js
@@ -30,10 +30,12 @@ window.addEventListener("load", function(){
            this.width = this.spriteWidth
            this.spriteX = this.collisionX - this.width * 0.5
            this.spriteY = this.collisionY - this.height * 0.5
+           this.frameX = 0
+           this.frameY = 0
         }
         // drawing  a player 
         draw(context){
-            context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight,  this.spriteX, this.spriteY, this.width, this.height)
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight,  this.spriteX, this.spriteY, this.width, this.height)
 
 
             context.beginPath();
@@ -54,6 +56,18 @@ window.addEventListener("load", function(){
         update(){
             this.dx = (this.game.mouse.x - this.collisionX)
             this.dy = (this.game.mouse.y - this.collisionY)
+            // sprite animation 
+            const angle = Math.atan2(this.dy, this.dx)
+            if(angle < -2.74 || angle > 2.74) this.frameY = 6
+            else if(angle < -1.96) this.frameY = 7
+            else if(angle < -1.17) this.frameY = 0
+            else if(angle < -0.39) this.frameY = 1
+            else if(angle < 0.39) this.frameY = 2
+            else if(angle < 1.17) this.frameY = 3
+            else if(angle < 1.96) this.frameY = 4
+            else if(angle < 2.74) this.frameY = 5
+            
+
             const distance =  Math.hypot(this.dy, this.dx)
 
             if(distance > this.speedModifier){
@@ -81,10 +95,6 @@ window.addEventListener("load", function(){
                     this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x
                     
                     this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y
-
-                    console.log(unit_x, unit_y, sumOfRadii)
-
-
                 }
             })
 
